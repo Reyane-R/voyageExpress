e17631e0785baafa283d8a6c796e9f04678fe7e5
Stephanenzb <36003820+Stephanenzb@users.noreply.github.com>
2019-11-27 14:53:34 +0100

    Add files via upload

--- admin/editDelete/deleteUtilisateur.php
+++ admin/editDelete/deleteUtilisateur.php
@@ -1,6 +1,6 @@
 <?php
     include "../../include.php";
-$id = $_GET['id'];
-$result = pg_query( "DELETE FROM utilisateur WHERE login=$id");
+$login = $_GET['login'];
+$result = pg_query( "DELETE FROM utilisateur WHERE login= '$login' ");
 header("Location:../registerUtilisateur.php");
 ?>
