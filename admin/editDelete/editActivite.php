a9c88c4e4f821f23b725724303a59fe046dfce47
Stephanenzb <36003820+Stephanenzb@users.noreply.github.com>
2019-11-29 03:56:53 +0100

    Add files via upload

--- admin/editDelete/editActivite.php
+++ admin/editDelete/editActivite.php
@@ -1,6 +1,6 @@
 <?php
-    include "../include.php";
-    include "../fonctions.php";
+    include "../../include.php";
+    include "../../fonctions.php";
     session_start();
   if (!$_SESSION['login']) {
 
@@ -9,7 +9,7 @@ header('Location:../index.php');
 
 } 
     $id = $_GET['id'];
-    $view = "Location: ./admin.php";
+    $view = "Location: ../registerActivite.php";
     $thisedit = "./edit.php?id=$id";
     $exist = false;
     $delete = "./delete.php?id=$id";
@@ -38,30 +38,38 @@ header('Location:../index.php');
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Voyage Express</title>
+   <!--Made with love by Mutiullah Samim -->
+   
+  <!--Bootsrap 4 CDN-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-        <link rel="stylesheet" href="style2.css">
-
-
-        <title>Voyage Express</title>
+  <!--Custom styles-->
+  <link rel="stylesheet" type="text/css" href="styles.css">
     </head>
     <body>
-    <div class="sidenav">
-         <div class="login-main-text">
-            <h2>Application<br> Login Page</h2>
-            <p>Login or register from here to access.</p>
+
+  <div class="container">
+    <div class="d-flex justify-content-center h-100">
+      <div class="card">
+
+         <div class="card-header">
+            <h3>Modification Activite</h3>
          </div>
-      </div>
-      <div class="main">
-         <div class="col-md-6 col-sm-12">
-            <div class="login-form">
+            <div class="card-body">
             <form action="<?php $thisedit ?>" method="post">
-            <div class="form-group">
-                     <label>User Name</label>
-                     <input type="text" name="name_activite" placeholder="<?php echo $name_activite ?>"/></li>
-                     <div class="form-group">
-                     <label>User Name</label>
+
+                      <div class="input-group form-group">
+                               <label><strong style="color:white;">Nom </strong></label>
+                               <input type="text" name="name_activite" placeholder="<?php echo $name_activite ?>"/></li>
+                      </div>
+
+                     
+                     <label><strong style="color:white;">Type</strong></label>
+                     <div class="input-group form-group">
                      <select name="type_activite" placeholder="<?php echo $type_activite ?>">
                                                     <option value="musee">musée</option>
                                                     <option value="parc d attraction">parc d'attraction</option>
@@ -70,18 +78,29 @@ header('Location:../index.php');
                                                     <option value="sport">sport</option>
                                                     <option value="reflexion">reflexion</option>
                                                     </select>
-                    <div class="form-group">
-                     <label>User Name</label> 
-                    <input type="text" name="adress_activite" placeholder="<?php echo $adress_activite ?>">                          
-                    <div class="form-group">
-                     <label>User Name</label> 
-                    <input type="text" name="city_activite" placeholder="<?php echo $city_activite?>"/>
-                    <div class="form-group">
-                     <label>User Name</label>  
+                    </div>
+
+                    <div class="input-group form-group">
+                     <label><strong style="color:white;">Adresse</strong></label> 
+                    <input type="text" name="adress_activite" placeholder="<?php echo $adress_activite ?>">
+                    </div>
+
+                    <div class="input-group form-group">
+                      <label><strong style="color:white;">Ville</strong></label> 
+                          <input type="text" name="city_activite" placeholder="<?php echo $city_activite?>"/>
+                    </div>
+
+
+                    <div class="input-group form-group">
+                     <label><strong style="color:white;">Prix</strong> </label>  
                     <input type="text" name="price_activite" placeholder="<?php echo $price_activite ?>"/>
+                    </div>
                 
-                <div class="center"><input type="submit" value="Submit"></div>
+                <div class="form-group">
+                  <input type="submit" value="Modifier" class="btn float-right login_btn">
+               </div>
             </form>
+            </div>
             </div>
          </div>
       </div>
