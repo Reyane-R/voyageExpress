752ba52c1bcaf70f54fd8306cf7c03b364550bac
Reyane-R <41648115+Reyane-R@users.noreply.github.com>
2019-12-02 14:40:53 +0100

    Add files via upload

--- user/utilisateur.php
+++ user/utilisateur.php
@@ -7,62 +7,41 @@ include('includes/navbar.php');
 <div class="container-fluid">
 <form action="">
 <!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
- <a href="#demo" data-toggle="collapse">Collapsible</a>
-
-<div id="demo" class="collapse">
-<div class="form-group">
-  <label for="sel1">Select list:</label>
-  <select multiple class="form-control" id="sel1">
-    <option>France</option>
-    <option>mali</option>
-    <option>belgiue</option>
-    <option>suisse</option>
-    <option>nepal</option>
-  </select>
-</div> 
-Lorem ipsum dolor text....
-</div> 
-  </div>
-  <div class="card shadow mb-4">
-  <div class="card-header py-3">
- <a href="#de" data-toggle="collapse">Collapsible</a>
-
-<div id="de" class="collapse">
-<form action="#">
-    <p class="range-field">
-      <input type="range" id="price-range" min="0" max="100" />
-    </p>
-  </form> 
-Lorem ipsum dolor text....
-</div> 
-  </div>
 
   <div class="card shadow mb-4">
   <div class="card-header py-3">
  <a href="#dem" data-toggle="collapse">Collapsible</a>
 
 <div id="dem" class="collapse">
+<p class="range-field">
+    <label for="price-range">Votre budget :</label>
+      <input type="range" id="price-range" min="0" max="1000000" />
+    </p> 
 <div class="form-group">
-  <label for="sel2">Select list:</label>
-  <select multiple class="form-control" id="sel2">
-    <option>1</option>
-    <option>2</option>
-    <option>3</option>
-    <option>4</option>
+  <label for="sel2">Choisir Votre Destination:</label>
+  <select  class="form-control" id="sel2">
+    <option>France</option>
+    <option>Belgique</option>
+    <option>Suisse</option>
+    <option>Russie</option>
+    <option>Allemagne</option>
+    <option>Angleterre</option>
+    <option>Croatie</option>
   </select>
+</div>
+<div class="form-group">
+  <label for="sel2">Categories:</label>
+  <select multiple class="form-control" id="sel2">
+    <option>Meteo</option>
+    <option>Locomotion</option>
+    <option>Nom de la ville</option>
+    <option>Hebergement</option>
+    <option>Activite</option>
+    <option>Point d'interet</option>
+    <option>Point d'interet</option>
+  </select>
+</div>
 </div> 
-Lorem ipsum dolor text....
-</div> 
-  </div>
-  <div class="card shadow mb-4">
-  <div class="card-header py-3">
-  <form action="#">
-    <p class="range-field">
-      <input type="range" id="price-range" min="0" max="100" />
-    </p>
-  </form>
   </div>
 
   <div class="card-body">
@@ -72,76 +51,66 @@ Lorem ipsum dolor text....
       <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
         <thead>
           <tr>
-            <th> ID </th>
-            <th> Username </th>
-            <th>Email </th>
-            <th>Password</th>
-            <th>EDIT </th>
-            <th>DELETE </th>
+            <th> city_name </th>
+            <th>name_activite </th>
+            <th> type_activite </th>
+            <th>adress_activite </th>
+            <th>Reserver</th>
           </tr>
         </thead>
         <tbody id="myTable">
      
           <tr>
-            <td> 2 </td>
-            <td> roro</td>
-            <td> lolo@example.com</td>
-            <td> *** </td>
+            <td> Paris </td>
+            <th>Disneyland Paris</th>
+            <td> parc d attraction</td>
+            <td> Boulevard de Parc, 77700 Coupvray</td>
             <td>
-                <form action="" method="post">
-                    <input type="hidden" name="edit_id" value="">
-                    <button  type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
-                </form>
-            </td>
-            <td>
-                <form action="" method="post">
-                  <input type="hidden" name="delete_id" value="">
-                  <button type="submit" name="delete_btn" class="btn btn-danger"> DELETE</button>
-                </form>
+              <form action="">
+              <input type="checkbox">
+              </form>
             </td>
           </tr>
           <tr>
-            <td> 3 </td>
-            <td> dqdqdq</td>
-            <td> dqdqd@example.com</td>
-            <td> *** </td>
+            <td> Paris</td>
+            <th>Musée d Orsay</th>
+            <td> musee</td>
+            <td> 1 Rue de la Légion d Honneur, 75007 Paris</td>
             <td>
-                <form action="" method="post">
-                    <input type="hidden" name="edit_id" value="">
-                    <button  type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
-                </form>
-            </td>
-            <td>
-                <form action="" method="post">
-                  <input type="hidden" name="delete_id" value="">
-                  <button type="submit" name="delete_btn" class="btn btn-danger"> DELETE</button>
-                </form>
+              <form action="">
+              <input type="checkbox">
+              </form>
             </td>
           </tr>
-        
           <tr>
-            <td> 1 </td>
-            <td> Funda of WEb IT</td>
-            <td> funda@example.com</td>
-            <td> *** </td>
+            <td> Paris </td>
+            <th>Parc des princes</th>
+            <td>sport</td>
+            <td>24 Rue du Commandant Guilbaud, 75016 Paris</td>
             <td>
-                <form action="" method="post">
-                    <input type="hidden" name="edit_id" value="">
-                    <button  type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
-                </form>
-            </td>
-            <td>
-                <form action="" method="post">
-                  <input type="hidden" name="delete_id" value="">
-                  <button type="submit" name="delete_btn" class="btn btn-danger"> DELETE</button>
-                </form>
+              <form action="">
+              <input type="checkbox">
+              </form>
             </td>
           </tr>
+          <tr>
+            <td> Paris </td>
+            <th>Spa Villa Thalgo</th>
+            <td>detente</td>
+            <td> 8 Avenue Raymond Poincaré, 75016 Paris</td>
+            <td>
+              <form action="">
+              <input type="checkbox">
+              </form>
+            </td>
+          </tr>
+
         
         
         </tbody>
       </table>
-
+      <input type="hidden" name="rever_id" value="">
+       <button  type="submit" name="rever_btn" class="btn btn-success"> RESERVER</button>
     </div>
   </div>
 </div>
